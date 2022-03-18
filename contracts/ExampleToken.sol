77022ce4c0fe0138c12ee36349519f6d997d082b
ququzone <xueping.yang@gmail.com>
2022-03-18 19:40:43 +0800

    format project

--- contracts/ExampleToken.sol
+++ contracts/ExampleToken.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ExampleToken is ERC20 {
-    constructor() ERC20("Example Token", "EXM") {
-        _mint(msg.sender, 10000 * 10**18);
-    }
+  constructor() ERC20("Example Token", "EXM") {
+    _mint(msg.sender, 10000 * 10**18);
+  }
 }
--- deploy/000-deploy-contract.ts
+++ deploy/000-deploy-contract.ts
@@ -1,20 +1,20 @@
 module.exports = async ({ ethers, getNamedAccounts, deployments }) => {
-    const { deploy } = deployments;
+  const { deploy } = deployments
 
-    const { log } = deployments;
-    const namedAccounts = await getNamedAccounts();
-    const { deployer } = namedAccounts;
+  const { log } = deployments
+  const namedAccounts = await getNamedAccounts()
+  const { deployer } = namedAccounts
 
-    const deployResult = await deploy('ExampleToken', {
-        from: deployer,
-        args: [],
-        log: true,
-        deterministicDeployment: false,
-    });
-    if (deployResult.newlyDeployed) {
-        log(
-            `contract ExampleToken deployed at ${deployResult.address} using ${deployResult.receipt.gasUsed} gas`
-        );
-    }
-};
-module.exports.tags = ['ExampleToken'];
+  const deployResult = await deploy("ExampleToken", {
+    from: deployer,
+    args: [],
+    log: true,
+    deterministicDeployment: false,
+  })
+  if (deployResult.newlyDeployed) {
+    log(
+      `contract ExampleToken deployed at ${deployResult.address} using ${deployResult.receipt.gasUsed} gas`
+    )
+  }
+}
+module.exports.tags = ["ExampleToken"]
