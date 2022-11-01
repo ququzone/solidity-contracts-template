25cad48bdee72d283dc6dee36a0b254d974bfc4f
ququzone <xueping.yang@gmail.com>
2022-11-01 10:28:28 +0800

    change openzeppelin to solmate

--- contracts/ExampleToken.sol
+++ contracts/ExampleToken.sol
@@ -2,10 +2,10 @@
 
 pragma solidity ^0.8.0;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "solmate/src/tokens/ERC20.sol";
 
 contract ExampleToken is ERC20 {
-    constructor() ERC20("Example Token", "EXM") {
+    constructor() ERC20("Example Token", "EXM", 18) {
         _mint(msg.sender, 10000 * 10**18);
     }
 }
