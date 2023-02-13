b57c1d00052397b390abeabf0c72c9c4752014a8
ququzone <xueping.yang@gmail.com>
2023-02-13 14:38:43 +0800

    merge foundry: first

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,18 +2,9 @@ import * as dotenv from "dotenv"
 import fs from "fs"
 import type { HardhatUserConfig } from "hardhat/config"
 import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-preprocessor"
 import "./tasks"
 
 dotenv.config()
-
-function getRemappings() {
-    return fs
-        .readFileSync("remappings.txt", "utf8")
-        .split("\n")
-        .filter(Boolean)
-        .map((line) => line.trim().split("="))
-}
 
 const config: HardhatUserConfig = {
     solidity: {
@@ -29,28 +20,22 @@ const config: HardhatUserConfig = {
             },
         ],
     },
+    gasReporter: {
+        enabled: process.env.REPORT_GAS === "true",
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+    },
     paths: {
         sources: "./src", // Use ./src rather than ./contracts as Hardhat expects
         cache: "./cache_hardhat", // Use a different cache for Hardhat than Foundry
     },
-    // This fully resolves paths for imports in the ./lib directory for Hardhat
-    // @ts-ignore
-    preprocess: {
-        eachLine: () => ({
-            transform: (line: string) => {
-                if (line.match(/^\s*import /i)) {
-                    getRemappings().forEach(([find, replace]) => {
-                        if (line.match(find)) {
-                            line = line.replace(find, replace)
-                        }
-                    })
-                }
-                return line
-            },
-        }),
-    },
     mocha: {
         timeout: 200000, // 200 seconds max for running tests
+    },
+    typechain: {
+        outDir: "typechain",
+        target: "ethers-v5",
     },
 }
 
